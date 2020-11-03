aa636c438fdc3c3af9c11c75a20b57d02ee451fc
Luis <luis.jauregui.fl@gmail.com>
2020-11-02 19:16:42 -0600

    initial commit

---
+++ frontend/components/bodyContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../styles/Body.scss'
+const BodyContainer = () => {
+    return (
+        <div className={styles.MainContainer}>
+            Hello from Body Container
+        </div>
+    )
+}
+
+export default BodyContainer
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BodyContainer from '../frontend/components/bodyContainer'
+
+const Main = () => {
+    return (
+        <div>
+            <BodyContainer/>
+        </div>
+    )
+}
+
+export default Main
